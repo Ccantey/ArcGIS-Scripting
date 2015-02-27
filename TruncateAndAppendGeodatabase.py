337539eed1f0c5676fc45ea698e8e8135d0228d6
Chris Cantey <cantey0701@gmail.com>
2015-02-27 13:37:24 -0600

    Create TruncateAndAppendGeodatabase.py

---
+++ TruncateAndAppendGeodatabase.py
@@ -0,0 +1,86 @@
+import arcpy
+import os
+
+##Read Water Database
+arcpy.env.workspace = "C:\Users\ccantey\Desktop\Local Government.gdb\WaterDistribution" ##works
+
+datasets = arcpy.ListDatasets(feature_type='feature')
+datasets = [''] + datasets if datasets is not None else []
+
+##Truncate feature classes
+try:
+  for ds in datasets:
+      for fc in arcpy.ListFeatureClasses(feature_dataset=ds):
+          path = os.path.join(arcpy.env.workspace, ds, fc)
+          ## Print all the feature classes
+          print path, " truncated"
+          arcpy.TruncateTable_management(path)
+except:
+    print "didnt't work"
+    print arcpy.GetMessages()
+
+
+# Set local variables
+inLocation = "C:\Users\ccantey\Desktop\Water_GISDATA\WaterSystem.mdb\Water_Distribution_Network"
+outLocation = "C:\Users\ccantey\Desktop\Local Government.gdb"
+schemaType = "NO_TEST"
+fieldMappings = ""
+subtype = ""
+
+print ""
+
+try:
+    # Process: Append the feature classes into the empty feature class   
+    wLateralLine = [inLocation + "\wLateralLine"]    
+    arcpy.Append_management(wLateralLine, outLocation + "\wLateralLine", schemaType, fieldMappings, subtype)
+    print "pass wLateralLine"
+    
+    # Process: Append the feature classes into the empty feature class   
+    wControlValve = [inLocation + "\wControlValve"]    
+    arcpy.Append_management(wControlValve, outLocation + "\wControlValve", schemaType, fieldMappings, subtype)
+    print "pass wControlValve"
+
+    # Process: Append the feature classes into the empty feature class   
+    wBreaks = [inLocation + "\wBreaks"]    
+    arcpy.Append_management(wBreaks, outLocation + "\wBreaks", schemaType, fieldMappings, subtype)
+    print "pass wBreaks"
+
+    # Process: Append the feature classes into the empty feature class   
+    wPressurizedMain = [inLocation + "\wPressurizedMain"]    
+    arcpy.Append_management(wControlValve, outLocation + "\wPressurizedMain", schemaType, fieldMappings, subtype)
+    print "pass wPressurizedMain"
+
+    # Process: Append the feature classes into the empty feature class   
+    wFitting = [inLocation + "\wFitting"]    
+    arcpy.Append_management(wFitting, outLocation + "\wFitting", schemaType, fieldMappings, subtype)
+    print "pass wFitting"
+
+    # Process: Append the feature classes into the empty feature class   
+    wHydrant = [inLocation + "\wHydrant"]    
+    arcpy.Append_management(wHydrant, outLocation + "\wHydrant", schemaType, fieldMappings, subtype)
+    print "pass wHydrant"
+
+    # Process: Append the feature classes into the empty feature class   
+    wManhole = [inLocation + "\wManhole"]    
+    arcpy.Append_management(wManhole, outLocation + "\wManhole", schemaType, fieldMappings, subtype)
+    print "pass wManhole"
+
+    # Process: Append the feature classes into the empty feature class   
+    wNetworkStructure = [inLocation + "\wNetworkStructure"]    
+    arcpy.Append_management(wNetworkStructure, outLocation + "\wNetworkStructure", schemaType, fieldMappings, subtype)
+    print "pass wNetworkStructure"
+
+    # Process: Append the feature classes into the empty feature class   
+    wSystemValve = [inLocation + "\wSystemValve"]    
+    arcpy.Append_management(wSystemValve, outLocation + "\wSystemValve", schemaType, fieldMappings, subtype)
+    print "pass wSystemValve"
+
+    # Process: Append the feature classes into the empty feature class   
+    Water_Distribution_Network_Junctions = [inLocation + "\Water_Distribution_Network_Junctions"]    
+    arcpy.Append_management(Water_Distribution_Network_Junctions, outLocation + "\Water_Distribution_Network_Junctions", schemaType, fieldMappings, subtype)
+    print "pass Water_Distribution_Network_Junctions"
+
+except:
+    # If an error occurred while running a tool print the messages
+    print arcpy.GetMessages()
+
