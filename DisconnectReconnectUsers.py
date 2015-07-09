45faa917589edb1add8d8248c03c8f75fc79775f
Chris Cantey <cantey0701@gmail.com>
2015-07-09 11:10:20 -0500

    Update DisconnectReconnectUsers.py

--- DisconnectReconnectUsers.py
+++ DisconnectReconnectUsers.py
@@ -1,5 +1,10 @@
 import arcpy
 
+# Many ArcGIS SDE maintenence tasks cannot be performed while users are connected to DB
+# This script will disconnect users and reconnect them after you run maintenence tasks 
+# One could just comment out the AcceptConnections method to do maintenence through catalog
+# then just run the accepting connections method once they are finished.
+
 #block new connections to the database.
 arcpy.AcceptConnections("Database Connections\your.sde", False)
 print 'Refusing Connections'
